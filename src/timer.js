bddce917e09d1859ee050f83d79cfc73b5ad6911
Drew Powers <drew@pow.rs>
2017-01-28 16:34:56 -0500

    Adds babel-loader

--- src/timer.js
+++ src/timer.js
@@ -1,10 +1,8 @@
 import moment from 'moment/min/moment.min';
 
-var timer = function() {
-  var time = moment().format('hh:mm:ss');
+const timer = () => {
+  const time = moment().format('hh:mm:ss');
   console.log(time);
 };
 
-export default function() {
-  setInterval(timer, 1000);
-}
+export default () => setInterval(timer, 1000);
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,15 @@ module.exports = {
   entry: {
     index: './index.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
